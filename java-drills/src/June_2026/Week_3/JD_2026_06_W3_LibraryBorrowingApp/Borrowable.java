64abf40f8beedfe5d0ec5551e3628878bab9beea
parkcwoo04-cyber <parkcwoo04@gmail.com>
2026-06-16 16:04:54 +0900

    JD_2026_06_W3_LibraryBorrowingSysytem problem solved

---
+++ java-drills/src/June_2026/Week_3/JD_2026_06_W3_LibraryBorrowingApp/Borrowable.java
@@ -0,0 +1,6 @@
+package src.June_2026.Week_3.JD_2026_06_W3_LibraryBorrowingApp;
+
+public interface Borrowable {
+    void borrowItem(LibraryMember member);
+    void returnItem();
+}
---
+++ java-drills/src/June_2026/Week_3/JD_2026_06_W3_LibraryBorrowingApp/LibraryItem.java
@@ -0,0 +1,37 @@
+package src.June_2026.Week_3.JD_2026_06_W3_LibraryBorrowingApp;
+
+public abstract class LibraryItem {
+    private String itemID;
+    private String title;
+    private boolean available;
+
+    public LibraryItem(String itemID, String title, boolean available) {
+        this.itemID = itemID;
+        this.title = title;
+        this.available = available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public abstract void showInfo();
+
+
+}
